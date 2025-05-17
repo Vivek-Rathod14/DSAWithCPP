833d8f9f57b6f6225dd8b6a2f3c22bc1ff4387dd
vivek-rathod14 <correct-email-linked-to-rathodvivek7260@gmail.com>
2025-05-17 20:33:16 +0530

     

--- Array/revese-array.cpp
+++ Array/revese-array.cpp
@@ -1,31 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-main(){
-		
-		int size,sum;
-		
-		cout<<"Enter Your size :";
-		cin>>size;
-		
-		int a[size]={},i;
-		
-		for(i=0;i<size;i++){
-			cin>>a[i];   
-			
-		}
-	
-		cout<<"\n\n";
-		
-		cout<<"Reverse Numbers :"<<endl;
-		
-// size= 0 1 2 3 4 
-//value= 5 4 3 2 1 
-	
-		for(i=size-1;i>=0;i--){
-			
-			cout<<a[i];
-	
-		}	
-		
+int main() {
+    int size;
+
+    cout << "Enter your size: ";
+    cin >> size;
+
+
+    int a[size];
+
+ 
+    for(int i = 0; i < size; i++) {
+        cin >> a[i];
+    }
+
+    cout << "\nReverse Numbers:" << endl;
+    for(int i = size - 1; i >= 1; i--) {
+        cout << a[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
+
